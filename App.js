6681f58b7d7c8151006e32c0e5121359b391e530
Mark Newman <mwnewman@umich.edu>
2020-10-13 23:11:49 -0400

    NYT solution

--- App.js
+++ App.js
@@ -37,8 +37,8 @@ class HomeScreen extends React.Component{
         <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details', {
-            itemId: 86,
-            otherParam: 'anything you want here',
+            itemId: 101,
+            otherParam: 'I want nothing',
           })}
         />
       </View>
